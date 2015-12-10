667cf9196de58e55b995228d2ef48388a51b700f
Juuso Ohra-aho <Juuso.ohra-aho@protonmail.com>
2015-12-10 14:30:01 +0200

    Tehtävä 2 a,b,c valmis

--- G7934_T2.aspx
+++ G7934_T2.aspx
@@ -8,8 +8,27 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
+    <div id="found" runat="server">
+        <asp:Button ID="btnVakkarit" runat="server" Text="Vakituiset" OnClick="btnVakkarit_Click" />
+        <asp:Button ID="btnMaaraikaset" runat="server" Text="Määräaikaiset" OnClick="btnMaaraikaset_Click" />
+        <asp:Button ID="btnElse" runat="server" Text="Muut" OnClick="btnElse_Click" />
+        <br />
+        <asp:Button ID="btnCountAll" runat="server" Text="Laske kaikki" OnClick="btnCountAll_Click" />
+        <asp:Button ID="btnCountVakkarit" runat="server" Text="Laske vakituiset" OnClick="btnCountVakkarit_Click" />
+        <asp:Button ID="btnCountMaaraikaset" runat="server" Text="Laske määräaikaiset" OnClick="btnCountMaaraikaset_Click" />
+        <asp:Button ID="btnCountElse" runat="server" Text="Laske muut" OnClick="btnCountElse_Click" />
+        <br />
+        <asp:Button ID="btnCountSalariesAll" runat="server" Text="Laske kaikkien palkat" OnClick="btnCountSalariesAll_Click" />
+        <asp:Button ID="btnCountSalariesVakkarit" runat="server" Text="Laske vakituisten palkat" OnClick="btnCountSalariesVakkarit_Click" />
+        <asp:Button ID="btnCountSalariesMaaraikaset" runat="server" Text="Laske määräaikaisten palkat" OnClick="btnCountSalariesMaaraikaset_Click" />
+        <asp:Button ID="btnCountSalariesElse" runat="server" Text="Laske muiden palkat" OnClick="btnCountSalariesElse_Click" />
+        <br />
+        <asp:Label ID="lblCount" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:GridView ID="gvData" runat="server"></asp:GridView>
+    </div>
+    <div id="notfound" runat="server" visible="false">
+        <h1>XML MÄÄRITTELYTIEDOSTOA EI LÖYDY!</h1>
     </div>
     </form>
 </body>
--- G7934_T2.aspx.cs
+++ G7934_T2.aspx.cs
@@ -4,10 +4,125 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
 
 public partial class G7934_T2 : System.Web.UI.Page
 {
+    String file;
+    DataSet ds;
+
     protected void Page_Load(object sender, EventArgs e)
+    {
+        file = WebConfigurationManager.AppSettings["file"];
+
+        if (file.Length > 0)
+        {
+            if (!IsPostBack)
+            {
+                ds = new DataSet();
+                ds.ReadXml(file);
+                gvData.DataSource = ds;
+                gvData.DataBind();
+            }
+        } 
+        else
+        {
+            found.Visible = false;
+            notfound.Visible = true;
+        }
+    }
+
+    private void refresh(String s)
+    {
+        ds = new DataSet();
+        ds.ReadXml(file);
+        var dv = ds.Tables[0].DefaultView;
+        dv.RowFilter = "tyosuhde = '"+ s + "'";
+        gvData.DataSource = dv;
+        gvData.DataBind();
+    }
+
+    protected void btnVakkarit_Click(object sender, EventArgs e)
+    {
+        refresh("vakituinen");
+    }
+
+    protected void btnMaaraikaset_Click(object sender, EventArgs e)
+    {
+        refresh("määräaikainen");
+    }
+
+    protected void btnElse_Click(object sender, EventArgs e)
+    {
+        refresh("vierailija");
+    }
+
+    protected void btnCountAll_Click(object sender, EventArgs e)
+    {
+        ds = new DataSet();
+        ds.ReadXml(file);
+        gvData.DataSource = ds;
+        gvData.DataBind();
+
+        lblCount.Text = "Kaikkien työntekijöiden määrä: " + gvData.Rows.Count.ToString();
+    }
+
+    protected void btnCountVakkarit_Click(object sender, EventArgs e)
+    {
+        refresh("vakituinen");
+
+        lblCount.Text = "Vakituisten työntekijöiden määrä: " + gvData.Rows.Count.ToString();
+    }
+
+    protected void btnCountMaaraikaset_Click(object sender, EventArgs e)
+    {
+        refresh("määräaikainen");
+
+        lblCount.Text = "Määräaikaisten työntekijöiden määrä: " + gvData.Rows.Count.ToString();
+    }
+
+    protected void btnCountElse_Click(object sender, EventArgs e)
+    {
+        refresh("vierailija");
+
+        lblCount.Text = "Muiden työntekijöiden määrä: " + gvData.Rows.Count.ToString();
+    }
+
+    protected void btnCountSalariesAll_Click(object sender, EventArgs e)
+    {
+        ds = new DataSet();
+        ds.ReadXml(file);
+        gvData.DataSource = ds;
+        gvData.DataBind();
+
+        var groupedData = from b in ds.Tables[0].AsEnumerable()
+                          group b by b.Field<string>("etunimi") into g
+                          select new
+                          {
+                              ChargeTag = g.Key,
+                              Count = g.Count(),
+                              ChargeSum = g.Sum(x => x.Field<int>("palkka"))
+                          };
+        int total = 0;
+        foreach (var item in groupedData)
+        {
+            total = item.ChargeSum;
+        }
+        lblCount.Text = "Kaikkien työntekijöiden palkka: " + total.ToString();
+    }
+
+    protected void btnCountSalariesVakkarit_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnCountSalariesMaaraikaset_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnCountSalariesElse_Click(object sender, EventArgs e)
     {
 
     }
