dc47d933f10a61197a3129ac18184bf2678a2443
Juuso Ohra-aho <Juuso.ohra-aho@protonmail.com>
2015-12-10 13:20:49 +0200

    Tehtävä 1 tehty

---
+++ G7934_T1b/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G7934_T1b
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AlphabetCounter counter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            counter = new AlphabetCounter();
+        }
+
+        private void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            if (counter.checkForContent(textBox.Text))
+            {
+                txtResult.Text = "";
+
+                int[] c = counter.countAlphabets(textBox.Text);
+
+                int total = 0;
+                int differents = 0;
+
+                for (int i = 0; i < (int)char.MaxValue; i++)
+                {
+                    if (c[i] > 0 && char.IsLetterOrDigit((char)i))
+                    {
+                        String newChar = (char)i + " = " + c[i] + "\n";
+                        txtResult.Text = txtResult.Text + newChar;
+                        total = total + c[i];
+                        differents++;
+                    }
+                }
+
+                txtResult.Text = txtResult.Text + "Yhteensä " + total + " merkkiä ja " + differents + " eri kirjainta.";
+            } else
+            {
+                txtResult.Text = "Syöte ei saa olla tyhjä eikä sisältää numeroita!";
+            }
+        }
+    }
+}
---
+++ G7934_T1b/alphabetCounter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G7934_T1b
+{
+    class AlphabetCounter
+    {
+        int[] chars = new int[(int)char.MaxValue];
+
+        public int[] countAlphabets(String s)
+        {
+            char[] letters = s.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                letters[i] = char.ToUpper(letters[i]);
+            }
+            s = new string(letters);
+            foreach (char c in s)
+            {
+                chars[(int)c]++;
+            }
+            return chars;
+        }
+
+        public bool checkForContent(String s)
+        {
+            if (s.Length > 0)
+            {
+                foreach (char c in s)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    } 
+}
